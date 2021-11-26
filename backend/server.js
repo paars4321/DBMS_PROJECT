09e5c45eb74ed6673271d9f49d856006ed34ac8a
paars4321 <parasmovaliya4321@gmail.com>
2021-11-26 17:06:55 +0530

    3rd

--- backend/server.js
+++ backend/server.js
@@ -31,17 +31,29 @@ app.use((req, res, next) => {
     next();
 });
 
-
 app.get("/users", (req, res) => {
     client
-    // console.log(req.query.uid)
-        .query(`select * FROM pr_db.user_information WHERE user_id=$1 AND password=$2`, [req.query.uid,req.query.password])
-        .then((data) => {
-            console.log(data.rows);
-            res.json(data.rows);
-        });
+        // console.log(req.query.uid)
+        .query(
+            `select * FROM pr_db.user_information WHERE email_id=$1 AND password=$2`,
+            [req.query.eid, req.query.password]
+        )
+        .then((data) => res.json(data.rows));
 });
 
+app.get("/centers", (req, res) => {
+    client
+        .query("select *FROM pr_db.data_center_information , pr_db.company_information where pr_db.data_center_information.owner_company=pr_db.company_information.company_id")
+        .then((data) => res.json(data.rows));
+});
+
+app.post("/", (req, res) => {
+    client
+        .query("INSERT INTO pr_db.user_information(name, email_id,gender,password,is_premium,contact_number) VALUES ($1, $2, $3, $4,$5,$6)", [req.body.name, req.body.email_id,req.body.gender,req.body.password,req.body.is_premium,req.body.contact_number])
+        // .query(`INSERT INTO table1 VALUES(${req.body.id}, ${req.body.name})`)
+        .then((data) => console.log(data.rows))
+        .catch((err) => console.log(err));
+});
 // app.post("/", (req, res) => {
 //     client
 //         .query("INSERT INTO table1(id, name) VALUES ($1, $2)", [req.body.id, req.body.name])
